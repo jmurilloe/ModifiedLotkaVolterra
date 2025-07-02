8e51ae1af13dad93645bae5c5341735614373bc0
Jaime Murillo Espitia <jmurilloe@unal.edu.co>
2025-07-02 15:22:52 -0500

    Advanced in metrics script.

--- metrics.R
+++ metrics.R
@@ -6,16 +6,28 @@ args <- commandArgs(trailingOnly = TRUE)
 if (length(args) != 1) {
   stop("Debe especificarse un archivo de entrada.")
 }
-file <- args[1]
+study <- args[1] # ex base
+N_i <- args[2] # ex 18
+N_j <- args[3] # ex 30
+total_features
 
-# Leer matriz
-V <- as.matrix(read.table(file, sep = " "))
+for(rep in 0:9){
+  inforep <- as.matrix(read.table(file, sep = " "))
+  repcharac <- as.character(rep)
+  Vname <- paste("results_"+study+"/"+N_i+"_"+N_j+"/V"+repcharac+".txt")
+  featuresname <- paste("results_"+study+"/"+N_i+"_"+N_j+"/features"+repcharac+".txt")
 
-# Calcular métricas
-H2 <- H2fun(V, H2_integer = FALSE)[1]
-mod <- computeModules(V)@likelihood
-nodf <- nested(V, method = "WNODA")
-connect <- networklevel(V, index = "weighted connectance")
+  # Leer matriz y features
+  V <- as.matrix(read.table(Vname, sep = " "))
+  features <- as.matrix(read.table(featuresname, sep = "\t"))
+
+  # Calcular métricas
+  H2 <- H2fun(V, H2_integer = FALSE)[1]
+  mod <- computeModules(V)@likelihood
+  nodf <- nested(V, method = "WNODA")
+  connect <- networklevel(V, index = "weighted connectance")
+
+}
 
 # Extraer directorio contenedor (e.g., results/6_13)
 dir <- dirname(file)
